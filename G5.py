f51ab252a257317258c19edada7fca4c8623cf3a
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-09-26 19:07:08 -0300

    Update G5.py

--- G5.py
+++ G5.py
@@ -1,35 +1,28 @@
-from Tela1 import SetupTela1
-from Tela2 import SetupTela2
-from Tela3 import SetupTela3
-from PosicaoTelas import Position
-
-class OpenMultWin:
-    def __init__(self):
-        self.ST1 = SetupTela1()
-        self.ST2 = SetupTela2()
-        self.ST3 = SetupTela3()
-
-    def Run(self):
-        self.ST2.Run()
-        self.ST1.Run()
-        self.ST3.Run()
+from Tela2 import SetupTela2 as ST2
+from Tela1 import SetupTela1 as ST1
+from Tela3 import SetupTela3 as ST3
+from PosicaoTelas import Position as Pos
+import time
 
 
 class PositionMultScreen:
     def __init__(self):
-        self.Pos = Position()
+        pass
 
     def Run(self):
+        time.sleep(2)
         for i in range (3):
+            Pos().EnableExtension()
             if i == 0:
-                self.Pos.Sent2First()
+                Pos().Sent2Third()
+                ST3().MaxWindow()
             if i == 1:
-                self.Pos.Sent2Third()
+                Pos().Sent2Third()
+                ST1().MaxWindow()
             if i == 2:
-                self.Pos.EnableExtension()
-            self.Pos.openFullScreen()
+                Pos().EnableExtension()
+            Pos().openFullScreen()
+            time.sleep(2)
 
-OMW = OpenMultWin()
 PMS = PositionMultScreen()
-OMW.Run()
-#PMS.Run()
+PMS.Run()
