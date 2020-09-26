e67d4df4eb05816834031ae2342aa3018752fa00
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-09-26 18:11:28 -0300

    Updated self.driver

--- Tela1.py
+++ Tela1.py
@@ -6,23 +6,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-options = Options()
-options.add_extension('extension_0_71_0_0.crx')
-driver = webdriver.Chrome(executable_path=r'./chromedriver.exe', options=options)
-driver.maximize_window()
-action = ActionChains(driver)
-
 class SetupTela1:
     def __init__(self):
-        pass
+        self.options = Options()
+        self.options.add_extension('extension_0_71_0_0.crx')
+        self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe', options=self.options)
+        self.driver.maximize_window()
+        self.action = ActionChains(self.driver)
 
     def access(self):
-        driver.get('https://g5.oxyn.com.br/')
+        self.driver.get('https://g5.oxyn.com.br/')
 
     def login(self):
-        login = driver.find_element_by_id('user_email')
-        password = driver.find_element_by_id('user_senha')
-        entrar = driver.find_element_by_xpath('//button[@type="submit"]')
+        login = self.driver.find_element_by_id('user_email')
+        password = self.driver.find_element_by_id('user_senha')
+        entrar = self.driver.find_element_by_xpath('//button[@type="submit"]')
         login.clear()
         login.send_keys('') #Add your e-mail
         password.clear()
@@ -30,21 +28,21 @@ class SetupTela1:
         entrar.click()
 
     def resumoDesvios(self):
-        driver.find_element_by_xpath("//li[@id='option1']//a[contains(text(),'Resumo de Desvios')]").click()
+        self.driver.find_element_by_xpath("//li[@id='option1']//a[contains(text(),'Resumo de Desvios')]").click()
 
     def Marcador(self):
-        markButton = WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='marker']//a[contains(text(),'Marcadores')]")))
+        markButton = WebDriverWait(self.driver,20).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='marker']//a[contains(text(),'Marcadores')]")))
         markButton.click()
-        marcador = driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Contrato de Manutenção')]")
-        filterBox = driver.find_element_by_id('filter')
-        action.drag_and_drop(marcador, filterBox).perform()
-        aplicar = driver.find_element_by_xpath("//div[@class='ui-dialog-buttonset']//button[contains(text(), 'Aplicar')]")
+        marcador = self.driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Contrato de Manutenção')]")
+        filterBox = self.driver.find_element_by_id('filter')
+        self.action.drag_and_drop(marcador, filterBox).perform()
+        aplicar = self.driver.find_element_by_xpath("//div[@class='ui-dialog-buttonset']//button[contains(text(), 'Aplicar')]")
         time.sleep(2)
         aplicar.click()
         time.sleep(1)
 
     def MaxWindow(self):
-        driver.maximize_window()
+        self.driver.maximize_window()
 
     def Run(self):
         self.access()
