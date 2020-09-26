7dc5b9eebd2f2a4f0d152b160783eb2abe496c47
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-09-26 20:29:09 -0300

    Update PosicaoTelas.py

--- PosicaoTelas.py
+++ PosicaoTelas.py
@@ -9,7 +9,6 @@ class Position:
         pyautogui.click(x=1803, y=50)
 
     def Sent2First(self):
-        self.EnableExtension()
         time.sleep(2)
         pyautogui.moveTo(x=1759,y=15, duration=0.5)
         pyautogui.dragTo(x=-960,y=100,duration=2.0)
