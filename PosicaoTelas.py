c593dffa7e4b3ab8e283a8d830897ab78d4597de
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-09-26 19:06:39 -0300

    Updated some coordinates

--- PosicaoTelas.py
+++ PosicaoTelas.py
@@ -5,18 +5,19 @@ class Position:
         pass
 
     def EnableExtension(self):
-        pyautogui.click(x=1780, y=50)
+        pyautogui.moveTo(x=1803, y=50, duration=0.25)
+        pyautogui.click(x=1803, y=50)
 
     def Sent2First(self):
         self.EnableExtension()
         time.sleep(2)
-        pyautogui.moveTo(x=1775,y=15)
+        pyautogui.moveTo(x=1759,y=15, duration=0.5)
         pyautogui.dragTo(x=-960,y=100,duration=2.0)
 
     def Sent2Third(self):
         self.EnableExtension()
         time.sleep(2)
-        pyautogui.moveTo(x=1775,y=15)
+        pyautogui.moveTo(x=1759,y=15, duration=0.5)
         pyautogui.dragTo(x=2880,y=100,duration=2.0)
 
     def openFullScreen(self):
